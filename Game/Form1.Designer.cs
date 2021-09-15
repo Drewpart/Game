25c599be18d62676bcdbd96492b1b237d8bbf04c
P4 <P4@DESKTOP-P2P0L1B>
2021-09-15 14:25:13 -0400

    Add project files.

---
+++ Game/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿
+namespace Game
+{
+    partial class Warzone
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pressPlay = new System.Windows.Forms.Label();
+            this.start = new System.Windows.Forms.Button();
+            this.play = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // pressPlay
+            // 
+            this.pressPlay.AutoSize = true;
+            this.pressPlay.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pressPlay.Location = new System.Drawing.Point(285, 114);
+            this.pressPlay.Name = "pressPlay";
+            this.pressPlay.Size = new System.Drawing.Size(257, 31);
+            this.pressPlay.TabIndex = 0;
+            this.pressPlay.Text = "Press Start To PLay";
+            // 
+            // start
+            // 
+            this.start.Font = new System.Drawing.Font("Sitka Display", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.start.Location = new System.Drawing.Point(240, 191);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(334, 111);
+            this.start.TabIndex = 1;
+            this.start.Text = "START";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // play
+            // 
+            this.play.AutoSize = true;
+            this.play.Font = new System.Drawing.Font("Yu Gothic Light", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.play.ForeColor = System.Drawing.Color.DarkOrange;
+            this.play.Location = new System.Drawing.Point(375, 145);
+            this.play.Name = "play";
+            this.play.Size = new System.Drawing.Size(66, 26);
+            this.play.TabIndex = 2;
+            this.play.Text = "Play!!!";
+            this.play.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Warzone
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkOrange;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.play);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.pressPlay);
+            this.Name = "Warzone";
+            this.Text = "Warzone";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label pressPlay;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Label play;
+    }
+}
+
---
+++ Game/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.Threading;
+
+namespace Game
+{
+    public partial class Warzone : Form
+    {
+        public Warzone()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SoundPlayer player = new SoundPlayer(Properties.Resources._523763__matrixxx__select_granted_06);
+            start.Hide();
+            pressPlay.Hide();
+            player.Play();
+
+            Refresh();
+            play.ForeColor = Color.Red;
+            Thread.Sleep(1000);
+            player.Play();
+            play.Text = " Game Starts In 3...";
+           
+            
+            Refresh();
+            Thread.Sleep(1000);
+            player.Play();
+            play.Text = " 2...";
+            
+            Refresh();
+            Thread.Sleep(1000);
+            player.Play();
+            play.Text = "1";
+            
+            Refresh();
+            Thread.Sleep(1000);
+            play.Text = "Start";
+
+            BackColor = Color.Green;
+            play.ForeColor = Color.White;
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
